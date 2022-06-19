6ddc3e0dcb21233b52ccb41a1409e0741258486d
fun <lljxx1@gmail.com>
2022-06-19 19:29:12 +0800

    remove

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { Detector } = require("./main");
-const whois = require("freewhois");
 const app = require("express")();
 const instance = new Detector();
+
 app.get("/detect", async (req, res) => {
   try {
     await instance.init();
@@ -13,50 +13,5 @@ app.get("/detect", async (req, res) => {
     });
   }
 });
-
-app.get("/getWhois", async (req, res) => {
-  try {
-    const data = await whois(req.query.link);
-    res.json({
-      data,
-    });
-  } catch (e) {
-    console.log("failed", e);
-    res.json({
-      error: e.toString(),
-    });
-  }
-});
-
-const { lookup } = require("./whois");
-const parseRawData = require("./parsed");
-
-app.get("/whois/lookup", async (req, res) => {
-  try {
-    const domain = req.query.domain;
-    const data = await new Promise((resolve, reject) => {
-      lookup(domain, (err, data) => {
-        if (err) {
-          return reject(rtt);
-        }
-        resolve(data);
-      });
-    })
-    const parsed = parseRawData(data, domain);
-    res.json({
-      data: parsed,
-    });
-  } catch (e) {
-    console.log("failed", e);
-    res.json({
-      error: e.toString(),
-    });
-  }
-});
-
-// console.log(lookup);
-// lookup("baidu.com", (err, data) => {
-//   console.log(err, data);
-// });
 
 app.listen(8080)
