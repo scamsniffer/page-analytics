dd486d3a073cc69ea7d08ea57a8c5b8b49bb3410
fun <lljxx1@gmail.com>
2022-07-04 13:22:41 +0800

    update

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const { Detector } = require("./main");
 const app = require("express")();
 const instance = new Detector({
+  args: ["--disable-dev-shm-usage", "--no-sandbox"],
   // headless: false,
-})
+});
 
 app.get("/detect", async (req, res) => {
   try {
